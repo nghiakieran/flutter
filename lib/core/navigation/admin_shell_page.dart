ab51eb7c1a5e08b403f04b7f8e3a4b93f25bf884
nghiakieran <lechinghia202@gmail.com>
2026-04-22 20:20:42 +0700

    Fix admin shell page

---
+++ lib/core/navigation/admin_shell_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'package:app_manager/features/admin/presentation/widgets/admin_bottom_navigation.dart';
+
+class AdminShellPage extends StatelessWidget {
+  const AdminShellPage({
+    super.key,
+    required this.child,
+    required this.currentRoute,
+  });
+
+  final Widget child;
+  final String currentRoute;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: child,
+      bottomNavigationBar: AdminBottomNavigation(currentRoute: currentRoute),
+    );
+  }
+}
